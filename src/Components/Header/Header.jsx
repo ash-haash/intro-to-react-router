7c924784f9bda1bb4ee42c59771d96c05a520d7b
ashrafur rahman farhad <itsfaarhaad@gmail.com>
2024-03-21 00:34:57 +0000

    NavLink, useNavigation(), useLocation()

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+
+import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
@@ -6,11 +7,11 @@ const Header = () => {
         <div>
             <h2>NavBar</h2>
             <nav>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/users'}>Users</Link>
-                <Link to={'/posts'}>Posts</Link>
-                <Link to={'/about'}>About</Link>
-                <Link to={'/contact'}>Contact Us</Link>
+                <NavLink to={'/'}>Home</NavLink>
+                <NavLink to={'/users'}>Users</NavLink>
+                <NavLink to={'/posts'}>Posts</NavLink>
+                <NavLink to={'/about'}>About</NavLink>
+                <NavLink to={'/contact'}>Contact Us</NavLink>
             </nav>
         </div>
     );
